ec5ebc1e56999825ff1645b80995c64a8666d06b
lyuz <lyuz0001@e.ntu.edu.sg>
2019-08-23 18:04:49 +0800

    new Parsing Class methods

---
+++ NTUTimetable v1.0/Utils/CourseIndex.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace ParsingClass
+{
+    
+    public class CourseIndex
+    {
+        public string courseName;
+        public string name;
+        public List<int> WeekSpan;
+        public bool[,,] classes = new bool[14, 7, 14];
+        public CourseIndex(string name, string courseName) {
+            this.name = name;
+            this.courseName = courseName;
+         
+        }
+        public CourseIndex() { }
+
+    }
+    
+}
